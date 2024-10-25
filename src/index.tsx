b580a9c649376ab40580910fee30b519ce2938ab
jcolago <jcolago2005@gmail.com>
2024-10-25 12:45:25 -0500

    Updated formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
-const container = document.getElementById('root')
-if (!container) throw new Error('Failed to find the root element')
-const root = createRoot(container)
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
