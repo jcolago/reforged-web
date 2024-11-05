ffbe88c6142cdda239e367c4b8737a5ad543b264
Joseph Colago <jcolago2005@gmail.com>
2024-11-05 11:50:32 -0600

    Updated root reducer and store

--- src/redux/reducers/_root.reducer.ts
+++ src/redux/reducers/_root.reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit"
 import user from "./user.reducer"
+import player from "./player.reducer"
 
 const rootReducer = combineReducers({
-user
+user,
+player
 })
 
 export default rootReducer
