293088b560a71b14c737454832d1f6bfe4f0d799
Joseph Colago <jcolago2005@gmail.com>
2024-11-08 15:05:40 -0600

    added monster reducer to root reducer

--- src/redux/reducers/_root.reducer.ts
+++ src/redux/reducers/_root.reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit"
 import user from "./user.reducer"
 import player from "./player.reducer"
+import monster from "./monster.reducer"
 
 const rootReducer = combineReducers({
 user,
-player
+player,
+monster
 })
 
 export default rootReducer
