bb2eefcc7dca3b1bad7e11d25852976789c7c4a6
Joseph Colago <jcolago2005@gmail.com>
2024-11-08 15:14:43 -0600

    Added condition reducer to root reducer

--- src/redux/reducers/_root.reducer.ts
+++ src/redux/reducers/_root.reducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit"
 import user from "./user.reducer"
 import player from "./player.reducer"
 import monster from "./monster.reducer"
+import condition from "./condition.reducer"
 
 const rootReducer = combineReducers({
 user,
 player,
-monster
+monster,
+condition
 })
 
 export default rootReducer
