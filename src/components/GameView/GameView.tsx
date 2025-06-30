5302135d5f98ebc4b139d88d231bc0b0b960f2b6
Joseph Colago <jcolago2005@gmail.com>
2025-06-29 21:48:23 -0500

    updated game view player card display

--- src/components/GameView/GameView.tsx
+++ src/components/GameView/GameView.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Typography, Container, Grid, Box, Paper } from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 import { RootState } from '../../redux/store';
 import { PlayerState } from '../../redux/reducers/player.reducer';
 import PlayerCard from '../PlayerCard/PlayerCard';
+import GlobalCard from '../../global/components/GlobalCard';
 
 const GameView: React.FC = () => {
   const players = useSelector((state: RootState) =>
@@ -11,58 +12,36 @@ const GameView: React.FC = () => {
   );
 
   return (
-    <Container maxWidth="xl">
-      <Box sx={{ py: 4 }}>
-        <Typography
-          variant="h4"
-          align="center"
-          sx={{
-            fontWeight: 'bold',
-            color: '#2c3e50',
-            textTransform: 'uppercase',
-            letterSpacing: '0.05em',
-            mb: 4,
-          }}
-        >
-          Game View
-        </Typography>
+    <div>
+      <Typography
+        variant="h4"
+        style={{
+          textAlign: 'center',
+          textDecoration: 'underline',
+          margin: '10px',
+        }}
+      >
+        Game View
+      </Typography>
 
-        <Paper
-          elevation={3}
-          sx={{
-            backgroundColor: 'rgb(128, 150, 191, .5)',
-            p: 3,
-            borderRadius: 2,
-          }}
-        >
-          {players.length === 0 ? (
-            <Box
-              sx={{
-                py: 8,
-                textAlign: 'center',
-                backgroundColor: 'rgba(255, 255, 255, 0.9)',
-                borderRadius: 1,
-              }}
-            >
-              <Typography variant="h6" color="text.secondary">
-                No characters are currently displayed.
-              </Typography>
-              <Typography variant="body1" color="text.secondary" sx={{ mt: 1 }}>
-                Add characters using the "Display" button in the player list.
-              </Typography>
-            </Box>
-          ) : (
-            <Grid container spacing={1}>
-              {players.map((player: PlayerState) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={player.id}>
-                  <PlayerCard player={player} />
-                </Grid>
-              ))}
+      <GlobalCard
+        width="95%"
+        style={{
+          border: '2px double black',
+          backgroundColor: 'rgb(128, 150, 191, .5)',
+          padding: '20px',
+          margin: 'auto',
+        }}
+      >
+        <Grid container spacing={3}>
+          {players.map((player: PlayerState) => (
+            <Grid item xs={12} md={6} key={player.id}>
+              <PlayerCard player={player} />
             </Grid>
-          )}
-        </Paper>
-      </Box>
-    </Container>
+          ))}
+        </Grid>
+      </GlobalCard>
+    </div>
   );
 };
 
