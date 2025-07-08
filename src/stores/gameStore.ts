1475fb9c0d8a714cde4021ce641424f1d0aa87a0
Joseph Colago <jcolago2005@gmail.com>
2025-07-08 00:50:31 -0500

    Added ffetchGames action to gameStore.ts

--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -37,5 +37,38 @@ interface GameStore{
     setCurrentGame: (game: Game | null) => void;
     clearError: () => void;
     reset: () => void;
-    
 }
+
+export const useGameStore = create<GameStore>()(
+    devtools(
+        immer((set, get) => ({
+            //initial state
+            games: [],
+            currentGame: null,
+            isLoading: false,
+            error: null,
+
+            //actions
+            fetchGames: async () => {
+                set((state) => {
+                    state.isLoading = true,
+                    state.error = null;
+                });
+
+                try {
+                    const response = await gameService.getGames();
+                    set((state) => {
+                        state.games = response.date;
+                        state.isLoading = false;
+                    })
+                } catch (error: any) {
+                    set((state) => {
+                        state.error = error.response?.data?.errors || 'Failed to fetch games.'
+                        state.isLoading = false;
+                    });
+                };
+            },
+            
+        }))
+    )
+)
