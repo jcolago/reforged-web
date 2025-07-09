bdc33da25c6d79b521f7df0c6529d194d47eac2e
Joseph Colago <jcolago2005@gmail.com>
2025-07-08 23:50:27 -0500

    added interfaces to monsterStore.ts

--- src/stores/monsterStore.ts
+++ src/stores/monsterStore.ts
@@ -25,3 +25,36 @@ export enum MonsterAlignment{
     NeutralEvil = "neutral_evil",
     ChaoticEvil = "chaotic_evil"
 }
+
+//Interfaces
+export interface MonsterState{
+    id: number;
+    name: string;
+    armor_class: number;
+    hit_points: number;
+    speed: number;
+    p_bonus: number;
+    resistances: string;
+    size: MonsterSize;
+    alignment: MonsterAlignment;
+    game_id: number;
+}
+
+interface MonsterStore{
+    //state
+    monsters: MonsterStore[];
+    currentMonster: MonsterState | null;
+    isLoading: boolean;
+    error: string | null;
+    displayedMonsters: MonsterState[];
+
+    //actions
+    fetchMonsters: () => Promise<void>;
+    addMonster: (monsterData: Omit<MonsterState, 'id'>) => Promise<MonsterState>;
+    updateMonster: (id: number, monsterData: Partial<MonsterState>) => Promise<void>;
+    removeMonster: (id: number) => Promise<void>;
+    toggleMonsterDisplay: (id: number) => void;
+    setCurrentMonster: (monster: MonsterState | null) => void;
+    clearError: () => void;
+    reset: () => void;
+}
